806d3e8d2b3cdb204bef68ac6b1852cc438b022b
P3rtang <tomverhulst21@gmail.com>
2023-01-21 15:25:27 +0100

    updated install script

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,22 +1,15 @@
+#!/bin/bash
+#
 $HOME=$(pwd)
-sudo pacman -S gdm sway swaybg waybar git lsd kitty rofi firefox unzip ttf-dejavu cifs-utils tmux\
-    npm base-devel
+
+sudo pacman -S --needed gdm sway swaybg waybar git lsd kitty rofi firefox unzip ttf-dejavu cifs-utils tmux\
+    npm base-devel pavucontrol
 sudo systemctl enable gdm
-mkdir .dotfiles
-git init --bare $HOME/.dotfiles
-gitbare config --local status.showUntrackedFiles no
-gitbare clone https://github.com/p3rtang/dotfiles
-cd .dotfiles
-fp -r . ~
-cd
-mkdir .local/share/fonts
-wget https://dtinth.github.io/comic-mono-font/ComicMono.ttf -P .local/share/fonts
-cd .local/share/fonts
-unzip fontawesome-free-5.15.4-desktop.zip
+
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-cd ~
-mkdir .aur
+
+mkdir -p .aur
 cd .aur
 git clone https://aur.archlinux.org/swaync.git
 cd swaync
@@ -24,3 +17,19 @@ makepkg -si
 git clone https://aur.archlinux.org/fastfetch.git
 cd fastfetch
 makepkg -si
+
+mkdir -p .dotfiles
+cd .dotfiles
+git init --bare
+git remote add origin https://github.com/p3rtang/dotfiles
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME fetch origin
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -f master
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+cd
+mkdir -p .local/share/fonts
+wget https://dtinth.github.io/comic-mono-font/ComicMono.ttf -P .local/share/fonts
+cd .local/share/fonts
+unzip fontawesome-free-5.15.4-desktop.zip
+cd ~
+mkdir -p Pictures/wallpapers
+wget -O ~/Pictures/wallpapers/factorio.jpg https://wallpapercave.com/wp/wp4616344.jpg

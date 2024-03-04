718aa5b165f319548d9bf69eab917a87f39c8cd4
p3rtang <tomverhulst21@gmail.com>
2024-03-04 20:55:46 +0100

    update: install file

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,58 +1,48 @@
 #!/bin/bash
-#
-$HOME=$(pwd)
 
-sudo pacman -Syu
-sudo pacman -S --needed --noconfirm gdm sway swaybg waybar git lsd kitty rofi firefox unzip ttf-dejavu cifs-utils tmux\
-    npm base-devel pavucontrol ranger
+set -e
+
+INSTALL=''
+declare -A osInfo;
+osInfo[/etc/debian_version]='apt-get install -y gdm3 sway waybar git exa kitty rofi unzip cifs-utils tmux pavucontrol'
+osInfo[/etc/arch-release]='pacman -Sy --needed --noconfirm gdm sway swaybg waybar git exa kitty rofi firefox unzip ttf-dejavu cifs-utils tmux npm base-devel pavucontrol'
+# osInfo[/etc/gentoo-release]=emerge
+# osInfo[/etc/SuSE-release]=zypp
+# osInfo[/etc/redhat-release]=yum
+# osInfo[/etc/alpine-release]=apk
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        INSTALL=${osInfo[$f]}
+    fi
+done
+
+sudo $INSTALL
 sudo systemctl enable gdm
+sudo systemctl enable gdm3
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-mkdir -p $HOME/.packages
-
-cd $HOME/.packages
-git clone https://aur.archlinux.org/swaync.git
-cd swaync
-makepkg -si --noconfirm
-
-cd $HOME/.packages
-git clone https://aur.archlinux.org/fastfetch.git
-cd fastfetch
-makepkg -si --noconfirm
-
-cd $HOME/.packages
-git clone https://aur.archlinux.org/wlr-randr.git
-cd wlr-randr
-makepkg -si --noconfirm
-
-cd $HOME/.packages
-git clone https://github.com/p3rtang/swaymenu
-cd swaymenu
-make install
-
 
 mkdir -p $HOME/.dotfiles
 cd $HOME/.dotfiles
 git init --bare
 git remote add origin https://github.com/p3rtang/dotfiles
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME fetch origin
-/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -f master
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -f desktop
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
 echo "setting up nvim"
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
-cd $HOME
-mkdir -p .local/share/fonts
+cd
+mkdir -p ~/.local/share/fonts
 wget https://dtinth.github.io/comic-mono-font/ComicMono.ttf -P .local/share/fonts
 
-cd $HOME/.local/share/fonts
-wget https://use.fontawesome.com/releases/v6.3.0/fontawesome-free-6.3.0-desktop.zip
+wget -O ~/.local/share/fonts https://use.fontawesome.com/releases/v6.3.0/fontawesome-free-6.3.0-desktop.zip
 unzip fontawesome-free-6.3.0-desktop.zip
 
-cd $HOME
 mkdir -p Pictures/wallpapers
 wget -O ~/Pictures/wallpapers/factorio.jpg https://wallpapercave.com/wp/wp4616344.jpg
 wget -O ~/Pictures/wallpapers/nausicaa.jpg http://www.wallpapersin4k.org/wp-content/uploads/2017/04/Nausicaa-Wallpaper-10.jpg

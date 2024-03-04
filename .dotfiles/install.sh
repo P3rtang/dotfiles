774e0aebc44a06e41e73a67f3241d443bdac0488
p3rtang <tomverhulst21@gmail.com>
2024-03-04 23:31:25 +0100

    feat: add fastfetch for debian

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -4,17 +4,22 @@ set -e
 
 INSTALL=''
 declare -A osInfo;
-osInfo[/etc/debian_version]='apt-get install -y gdm3 sway waybar git exa kitty rofi unzip cifs-utils tmux pavucontrol neovim curl'
-osInfo[/etc/arch-release]='pacman -Sy --needed --noconfirm gdm sway swaybg waybar git exa kitty rofi firefox unzip ttf-dejavu cifs-utils tmux npm base-devel pavucontrol neovim curl'
+osInfo[/etc/debian_version]=debian
+osInfo[/etc/arch-release]=arch
 # osInfo[/etc/gentoo-release]=emerge
 # osInfo[/etc/SuSE-release]=zypp
 # osInfo[/etc/redhat-release]=yum
 # osInfo[/etc/alpine-release]=apk
 
+declare -A Install;
+osInfo[debian]='apt-get install -y gdm3 sway waybar git exa kitty rofi unzip cifs-utils tmux pavucontrol neovim curl playerctl nala '
+osInfo[arch]='pacman -Sy --needed --noconfirm gdm sway swaybg waybar git exa kitty rofi firefox unzip ttf-dejavu cifs-utils tmux npm base-devel pavucontrol neovim curl playerctl fastfetch'
+
 for f in ${!osInfo[@]}
 do
     if [[ -f $f ]];then
-        INSTALL=${osInfo[$f]}
+        OS_NAME=${osInfo[$f]}
+        INSTALL=${Install[$OS_NAME]}
     fi
 done
 
@@ -46,3 +51,15 @@ unzip -o ~/.local/share/fonts/fontawesome-free-6.3.0-desktop.zip -d $HOME/.local
 mkdir -p Pictures/wallpapers
 curl https://wallpapercave.com/wp/wp4616344.jpg --create-dirs -o ~/Pictures/wallpapers/factorio.jpg
 curl https://i.imgur.com/Jj0zk7c.jpeg --create-dirs -o ~/Pictures/wallpapers/nausicaa.jpg
+
+# on debian get fastfetch from github
+if [[ $OS_NAME = "debian" ]];then
+    curl -L --create-dirs -o $HOME/.packages/fastfetch/fastfetch.deb \
+    $(curl -L \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest \
+        | jq -r '.assets[] | select(.name=="fastfetch-linux-amd64.deb").browser_download_url' \
+    )
+    sudo apt-get install -y ~/.packages/fastfetch/fastfetch.deb
+fi

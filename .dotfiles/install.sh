9ef27629c5f6705946a53c75c5cb30c69a0d0d90
p3rtang <tomverhulst21@gmail.com>
2024-03-04 21:13:14 +0100

    fix: install packer overwrite old

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -19,11 +19,11 @@ do
 done
 
 sudo $INSTALL
-sudo systemctl enable gdm
-sudo systemctl enable gdm3
+sudo systemctl enable gdm || sudo systemctl enable gdm3
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# install packer nvim
+rm ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 mkdir -p .aur
 cd .aur

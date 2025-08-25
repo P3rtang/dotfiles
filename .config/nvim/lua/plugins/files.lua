779dd4ed8fc7cc3b7b54131859105bc9b3e55ac9
p3rtang <tomverhulst21@gmail.com>
2025-08-25 16:26:39 +0200

    fix: tree-sitter requires config callback

--- .config/nvim/lua/plugins/files.lua
+++ .config/nvim/lua/plugins/files.lua
@@ -1,74 +1,78 @@
 return {
-    {
-        "stevearc/oil.nvim",
-        opts = {
-            default_file_explorer = true,
-            delete_to_trash = false,
-            columns = { "icon", "permissions", "size", "mtime" },
-            view_options = {
-                show_hidden = true,
-            },
-            git = {
-                -- Return true to automatically git add/mv/rm files
-                add = function()
-                    return false
-                end,
-                mv = function()
-                    return true
-                end,
-                rm = function()
-                    return false
-                end,
-            },
-            float = {
-                -- Padding around the floating window
-                padding = 8,
-                -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-                max_width = 0.64,
-                max_height = 0.80,
-                border = "rounded",
-                win_options = {
-                    winblend = 0,
-                },
-                -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
-                get_win_title = nil,
-                -- preview_split: Split direction: "auto", "left", "right", "above", "below".
-                preview_split = "right",
-            },
-            keymaps = {
-                ["q"] = "actions.close",
-                ["<Esc>"] = "actions.close",
+	{
+		"stevearc/oil.nvim",
+		opts = {
+			default_file_explorer = true,
+			delete_to_trash = false,
+			columns = { "icon", "permissions", "size", "mtime" },
+			view_options = {
+				show_hidden = true,
+			},
+			git = {
+				-- Return true to automatically git add/mv/rm files
+				add = function()
+					return false
+				end,
+				mv = function()
+					return true
+				end,
+				rm = function()
+					return false
+				end,
+			},
+			float = {
+				-- Padding around the floating window
+				padding = 8,
+				-- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+				max_width = 0.64,
+				max_height = 0.80,
+				border = "rounded",
+				win_options = {
+					winblend = 0,
+				},
+				-- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+				get_win_title = nil,
+				-- preview_split: Split direction: "auto", "left", "right", "above", "below".
+				preview_split = "right",
+			},
+			keymaps = {
+				["q"] = { "actions.close", desc = "Oil: Close" },
+				["<Esc>"] = { "actions.close", desc = "Oil: Close" },
 
-                ["<CR>"] = "actions.select",
-                ["<C-v>"] = { "actions.select", opts = { vertical = true } },
-                ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
-                ["<C-t>"] = { "actions.select", opts = { tab = true } },
-                ["<C-p>"] = "actions.preview",
-                ["<C-c>"] = { "actions.close", mode = "n" },
-                ["<C-l>"] = "actions.refresh",
+				["<CR>"] = { "actions.select", desc = "Oil: Select" },
+				["<C-v>"] = { "actions.select", opts = { vertical = true }, desc = "Oil: Select and split vertical" },
+				["<C-h>"] = {
+					"actions.select",
+					opts = { horizontal = true },
+					desc = "Oil: Select and split horizontal",
+				},
+				["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Oil: Select and open in new tab" },
+				["<C-p>"] = { "actions.preview", desc = "Oil: Toggle buffer preview" },
+				["<C-l>"] = "actions.refresh",
+				["<C-c>"] = { "actions.close", mode = "n", desc = "Oil: Close" },
 
-                ["_"] = { "actions.open_cwd", mode = "n" },
-                ["`"] = { "actions.cd", mode = "n" },
-                ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+				["_"] = { "actions.open_cwd", mode = "n", desc = "Oil: open cwd (root directory)" },
+				["`"] = { "actions.cd", mode = "n", desc = "Oil: change directory" },
+				["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n", desc = "Oil: set temp cwd" },
 
-                ["g?"] = { "actions.show_help", mode = "n" },
-                ["gs"] = { "actions.change_sort", mode = "n" },
-                ["gx"] = "actions.open_external",
+				["g?"] = { "actions.show_help", mode = "n", desc = "Oil: show help" },
+				["gs"] = { "actions.change_sort", mode = "n", desc = "Oil: change sorting" },
+				["gx"] = { "actions.open_external", desc = "Oil: open external file" },
 
-                ["g."] = { "actions.toggle_hidden", mode = "n" },
-                ["g\\"] = { "actions.toggle_trash", mode = "n" },
-            },
-            -- Set to false to disable all of the above keymaps
-            use_default_keymaps = false,
-        },
-        keys = {
-            {
-                "-",
-                function()
-                    require("oil").open_float(nil, { preview = { vertical = true } })
-                end,
-                desc = "Open parent directory",
-            },
-        },
-    },
+				["g."] = { "actions.toggle_hidden", mode = "n", desc = "Oil: toggle hidden files" },
+				["g\\"] = { "actions.toggle_trash", mode = "n", desc = "Oil: toggle show trash" },
+			},
+			-- Set to false to disable all of the above keymaps
+			use_default_keymaps = false,
+		},
+		keys = {
+			{
+				"-",
+				function()
+					require("oil").open_float(nil, { preview = { vertical = true } })
+				end,
+				desc = "Oil: open parent directory",
+			},
+		},
+	},
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,161 +1,166 @@
 local border = {
-    { "🭽", "FloatBorder" },
-    { "▔", "FloatBorder" },
-    { "🭾", "FloatBorder" },
-    { "▕", "FloatBorder" },
-    { "🭿", "FloatBorder" },
-    { "▁", "FloatBorder" },
-    { "🭼", "FloatBorder" },
-    { "▏", "FloatBorder" },
+	{ "🭽", "FloatBorder" },
+	{ "▔", "FloatBorder" },
+	{ "🭾", "FloatBorder" },
+	{ "▕", "FloatBorder" },
+	{ "🭿", "FloatBorder" },
+	{ "▁", "FloatBorder" },
+	{ "🭼", "FloatBorder" },
+	{ "▏", "FloatBorder" },
 }
 
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-    opts = opts or {}
-    opts.border = opts.border or border
-    return orig_util_open_floating_preview(contents, syntax, opts, ...)
+	opts = opts or {}
+	opts.border = opts.border or border
+	return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
 
 local on_attach = function(_, bufnr)
-    -- Enable function signatures
-    require("lsp_signature").setup()
-    require("lsp_signature").on_attach(signature_setup, bufnr)
+	-- Enable function signatures
+	require("lsp_signature").setup()
+	require("lsp_signature").on_attach(signature_setup, bufnr)
 
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set("n", "<space>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, bufopts)
-    vim.keymap.set("n", "]g", vim.diagnostic.goto_next, bufopts)
-    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-    vim.keymap.set("n", "<space>f", function()
-        vim.lsp.buf.format({ async = true })
-    end, bufopts)
-    vim.keymap.set("n", "E", vim.diagnostic.open_float)
-    vim.keymap.set("n", "K", function()
-        vim.lsp.buf.hover()
-        vim.lsp.buf.hover()
-    end)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, bufopts)
+	vim.keymap.set("n", "]g", vim.diagnostic.goto_next, bufopts)
+	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	vim.keymap.set("n", "<space>f", function()
+		vim.lsp.buf.format({ async = true })
+	end, bufopts)
+	vim.keymap.set("n", "E", vim.diagnostic.open_float)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+		vim.lsp.buf.hover()
+	end)
 end
 
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            auto_install = true,
-            highlight = { enable = true },
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        name = "lspconfig",
-        config = function()
-            require("lspconfig").gleam.setup({ virtual_text = true })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                callback = function()
-                    vim.lsp.buf.format()
-                end,
-            })
-        end,
-    },
-    {
-        "stevearc/conform.nvim",
-        event = { "BufWritePre" },
-        cmd = { "ConformInfo" },
-        opts = {
-            format_on_save = {
-                -- These options will be passed to conform.format()
-                timeout_ms = 200,
-                lsp_format = true,
-            },
-            formatters_by_ft = {
-                lua = { "stylua" },
-                javascript = { "prettier", stop_after_first = true },
-                typescript = { "prettier", stop_after_first = true },
-                sql = { "pg_format" },
-            },
-        },
-    },
-    { "ray-x/lsp_signature.nvim" },
-    {
-        "williamboman/mason.nvim",
-        name = "mason",
-        dependencies = "lspconfig",
-        opts = {},
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        name = "mason-lspconfig",
-        dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
-        },
-        config = function()
-            local lspconfig = require("lspconfig")
-            require("mason-lspconfig").setup({})
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "VeryLazy" },
+		opts = {
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		name = "lspconfig",
+		config = function()
+			require("lspconfig").gleam.setup({ virtual_text = true })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				callback = function()
+					vim.lsp.buf.format()
+				end,
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		opts = {
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 200,
+				lsp_format = true,
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettier", stop_after_first = true },
+				typescript = { "prettier", stop_after_first = true },
+				sql = { "pg_format" },
+			},
+		},
+	},
+	{ "ray-x/lsp_signature.nvim" },
+	{
+		"williamboman/mason.nvim",
+		name = "mason",
+		dependencies = "lspconfig",
+		opts = {},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		name = "mason-lspconfig",
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+			require("mason-lspconfig").setup({})
 
-            vim.lsp.config("*", {
-                on_attach = on_attach,
-            })
+			vim.lsp.config("*", {
+				on_attach = on_attach,
+			})
 
-            vim.lsp.config("rust_analyzer", {
-                on_attach = on_attach,
-                settings = {
-                    ["rust-analyzer"] = {
-                        ["cargo"] = {
-                            ["allFeatures"] = true,
-                        },
-                    },
-                },
-            })
+			vim.lsp.config("rust_analyzer", {
+				on_attach = on_attach,
+				settings = {
+					["rust-analyzer"] = {
+						["cargo"] = {
+							["allFeatures"] = true,
+						},
+					},
+				},
+			})
 
-            vim.lsp.config("gopls", {
-                on_attach = on_attach,
-                cmd = { "gopls" },
-                filetypes = { "go", "gomod", "gowork", "gotmpl" },
-                root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
-                settings = {
-                    gopls = {
-                        completeUnimported = true,
-                        usePlaceholders = true,
-                        analyses = {
-                            unusedparams = true,
-                        },
-                    },
-                },
-            })
+			vim.lsp.config("gopls", {
+				on_attach = on_attach,
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+				settings = {
+					gopls = {
+						completeUnimported = true,
+						usePlaceholders = true,
+						analyses = {
+							unusedparams = true,
+						},
+					},
+				},
+			})
 
-            vim.lsp.config("lua_ls", {
-                on_attach = on_attach,
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = "LuaJIT",
-                        },
-                        diagnostics = {
-                            globals = { "vim" },
-                        },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("", true),
-                            checkThirdParty = false,
-                        },
-                        telemetry = {
-                            enable = false,
-                        },
-                    },
-                },
-            })
-        end,
-    },
+			vim.lsp.config("lua_ls", {
+				on_attach = on_attach,
+				settings = {
+					Lua = {
+						runtime = {
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
+		end,
+	},
 }

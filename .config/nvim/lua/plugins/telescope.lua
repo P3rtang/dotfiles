b19c87bb35af98a139b6628e40946a11fe498f9a
P3rtang <tom@puls.be>
2025-08-26 09:06:20 +0200

    feat: update work config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,13 @@
 return {
-	{ "nvim-telescope/telescope.nvim", tag = "0.1.8", depenencies = "nvim-lua/plenary.nvim" },
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { 
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        depenencies = 'nvim-lua/plenary.nvim',
+        opts = {
+            defaults = {
+                layout_strategy = 'vertical',
+            }
+        } 
+    },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 }
